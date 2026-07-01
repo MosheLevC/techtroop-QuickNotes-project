2b739d53b7ed92481cd9f18b19cd903d2f2801b4
Moshe Lev Ceder <navkc11@gmail.com>
2026-07-01 03:15:49 +0300

    feat: enhance NoteForm to handle note submission from App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import NoteForm from "./NoteForm";
 
 function App() {
   const [currentNote, setCurrentNote] = useState({ id: null, title: "", body: "", category: "" });
+  const [notes, setNotes] = useState({});
 
   return (
     <div className="container">
-      <NoteForm title={currentNote.title} body={currentNote.body} category={currentNote.category} />
+      <NoteForm
+        title={currentNote.title}
+        body={currentNote.body}
+        category={currentNote.category}
+        handleOnSubmit={(newNote) => {
+          setNotes(newNote);
+        }}
+      />
     </div>
   );
 }
--- src/NoteForm.jsx
+++ src/NoteForm.jsx
@@ -1,11 +1,18 @@
 import { Button, Paper, TextInput } from "@mantine/core";
 
-const NoteForm = ({ title, body, category }) => {
+const NoteForm = ({ id, title, body, category, handleOnSubmit }) => {
+  let currentTitle = title;
+  let currentBody = body;
+  let currentCategory = category;
   return (
     <Paper shadow="xs" withBorder p="xl" className="note-form">
       <TextInput placeholder="title" value={title} />
       <textarea>{body}</textarea>
-      <Button variant="default" fullWidth>
+      <Button
+        onClick={() => handleOnSubmit({ id: id ? id : null, title: title ? title : "", body: body, category: category ? category : "" })}
+        variant="default"
+        fullWidth
+      >
         Add
       </Button>
     </Paper>
