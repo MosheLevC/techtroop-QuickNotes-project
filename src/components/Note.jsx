46068a76d4b8c4621af50d9eecb8ec335537df9c
Moshe Lev Ceder <navkc11@gmail.com>
2026-07-01 18:49:34 +0300

    feat: enhance Note component layout with improved text alignment and styling

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,11 +3,13 @@ import { CloseButton, Flex, Paper, Text } from "@mantine/core";
 const Note = ({ id, createdAt, title, body, category, handleRemove }) => {
   return (
     <Paper shadow="xs" withBorder p="xl" className="note">
-      <Flex align="center">
+      <Flex align="center" justify="space-between">
         <Text size="xs">{createdAt}</Text>
         <CloseButton onClick={handleRemove} />
       </Flex>
-      <Text size="md">{title}</Text>
+      <Text size="md" fw="bold">
+        {title}
+      </Text>
       <Text size="sm">{body}</Text>
     </Paper>
   );
