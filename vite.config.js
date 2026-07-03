f79a02a6fc3b3ff65d68688751a9412431e977af
Moshe Lev Ceder <navkc11@gmail.com>
2026-07-03 05:19:54 +0300

    fix: update base path in Vite config for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/techtroop-QuickNotes-project/",
   plugins: [react()],
   resolve: {
     alias: {
