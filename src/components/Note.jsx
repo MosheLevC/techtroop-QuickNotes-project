7927deeabd86566510f39b72c6eae55d756ae8ab
Moshe Lev Ceder <navkc11@gmail.com>
2026-07-03 04:48:04 +0300

    refactor: rearrange Note component structure for click bubbling to work

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,36 +7,38 @@ const Note = ({ selectedNote, handleRemove, handleUpdate }) => {
   const { title, body, createdAt } = selectedNote;
 
   return (
-    <Paper
-      shadow="xs"
-      withBorder
-      p="md"
-      className="note"
-      onClick={open}
-      style={{ cursor: "pointer" }}
-    >
+    <>
       <NoteModal
         opened={opened}
         close={close}
         selectedNote={selectedNote}
         handleUpdate={handleUpdate}
       />
-      <Flex align="center" justify="space-between">
-        <Text size="xs">{createdAt}</Text>
-        <CloseButton
-          onClick={(e) => {
-            e.stopPropagation();
-            handleRemove();
-          }}
-        />
-      </Flex>
-      <Text size="md" fw="bolder" truncate>
-        {title}
-      </Text>
-      <Text size="sm" lineClamp={title ? 4 : 5}>
-        {body}
-      </Text>
-    </Paper>
+      <Paper
+        shadow="xs"
+        withBorder
+        p="md"
+        className="note"
+        onClick={open}
+        style={{ cursor: "pointer" }}
+      >
+        <Flex align="center" justify="space-between">
+          <Text size="xs">{createdAt}</Text>
+          <CloseButton
+            onClick={(e) => {
+              e.stopPropagation();
+              handleRemove();
+            }}
+          />
+        </Flex>
+        <Text size="md" fw="bolder" truncate>
+          {title}
+        </Text>
+        <Text size="sm" lineClamp={title ? 4 : 5}>
+          {body}
+        </Text>
+      </Paper>
+    </>
   );
 };
 export default Note;
